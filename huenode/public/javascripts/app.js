5031d88cf1d7750e3b96023cb7cd261b13e32bc2
Montoya Maria <montoym1@niksula.hut.fi>
2014-05-14 23:18:50 +0300

    Last update UI

--- huenode/public/javascripts/app.js
+++ huenode/public/javascripts/app.js
@@ -6,11 +6,29 @@ var ip = "http://192.168.1.100";
 var tmp;
 var lightsNumber;
 
-function setHue1(){
+/***********************************************************
+* REFERENCE:http://snipplr.com/view.php?codeview&id=37974  *
+************************************************************/
+function hexToRGB(hex) {
+  if (hex[0]=="#") hex=hex.substr(1);
+  if (hex.length==3) {
+    var temp=hex; hex='';
+    temp = /^([a-f0-9])([a-f0-9])([a-f0-9])$/i.exec(temp).slice(1);
+    for (var i=0;i<3;i++) hex+=temp[i]+temp[i];
+  }
+  var triplets = /^([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i.exec(hex).slice(1);
+  return {
+    red:   parseInt(triplets[0],16),
+    green: parseInt(triplets[1],16),
+    blue:  parseInt(triplets[2],16)
+  }
+}
 
+function setHue1(){
     var color = $("#inputColor1").val();
     var brightness = $("#inputBright1").val();
     var inputSat = $("#inputSat1").val();
+	var rgbc = hexToRGB(color);
 	var lights = ip + "/api/newdeveloper/lights/1/state";
 	var http = new XMLHttpRequest();
 
@@ -25,6 +43,7 @@ function setHue2(){
     var color = $("#inputColor2").val();
     var brightness = $("#inputBright2").val();
     var inputSat = $("#inputSat2").val();
+	var rgbc =hexToRGB(color);
     var lights = ip + "/api/newdeveloper/lights/2/state";
     var http = new XMLHttpRequest();
 
@@ -38,6 +57,7 @@ function setHue3(){
     var color = $("#inputColor3").val();
     var brightness = $("#inputBright3").val();
     var inputSat = $("#inputSat3").val();
+	var rgbc =hexToRGB(color);
     var lights = ip + "/api/newdeveloper/lights/3/state";
     var http = new XMLHttpRequest();
 
